88233ca09a812eb73ab1cc645f58c706ce920e14
HakierGrzonzo <grzes869@gmail.com>
2019-06-15 20:18:41 +0200

    Update storyFetch.py

--- storyFetch.py
+++ storyFetch.py
@@ -1,13 +1,31 @@
 
 import http.client
 import json
+import time
 
-connection = http.client.HTTPSConnection('www.reddit.com')
+debug = True
 
-headers = { b'User-Agent' : b'super moody reddit2youtube bot by /u/HakierGrzonzo' }
+def GetStories(subreddit, mode, limit):
+	connection = http.client.HTTPSConnection('www.reddit.com')
+	headers = { b'User-Agent' : b'super moody reddit2youtube bot by /u/HakierGrzonzo' }
+	connection.request('GET',"/r/" + subreddit + "/" + mode + "/.json?limit="+str(limit), headers)
+	response = connection.getresponse()
+	return response.read().decode()
+
+def NoOfStories(stories):
+	return len(stories.get("data").get("children"))
+
+def printTexts(stories):
+	print("fetched " + str(NoOfStories(stories)) + " stories:")
+	for title in stories.get("data").get("children"):
+		print("# " + title.get("data").get("title"))
+		if title.get("data").get("selftext") != "":
+ 			print(title.get("data").get("selftext"))
+		print("----------")
+
+if __name__ == '__main__':
+	stories = json.loads(GetStories("tifu", "top", 10))
+	printTexts(stories)
 
 
-connection.request('GET',"/r/entitledparents/top/.json?count=20", headers)
-
-response = connection.getresponse()
-print(response.read().decode())
+	
