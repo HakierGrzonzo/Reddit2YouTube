b0bb75818f1f21569dc438fd331809d3bc632008
HakierGrzonzo <grzes869@gmail.com>
2019-06-15 19:04:05 +0200

    it works

--- storyFetch.py
+++ storyFetch.py
@@ -4,7 +4,7 @@ import json
 
 connection = http.client.HTTPSConnection('www.reddit.com')
 
-headers = { 'User-Agent' : 'super moody reddit2youtube bot by /u/HakierGrzonzo' }
+headers = { b'User-Agent' : b'super moody reddit2youtube bot by /u/HakierGrzonzo' }
 
 
 connection.request('GET',"/r/entitledparents/top/.json?count=20", headers)
