0f65521669dcc68331c3c59b6e88bf5cb89d8eee
HakierGrzonzo <grzes869@gmail.com>
2019-06-15 22:19:46 +0200

    made some text handling code

--- storyFetch.py
+++ storyFetch.py
@@ -2,6 +2,8 @@
 import http.client
 import json
 import time
+import os
+from PIL import Image, ImageDraw
 
 debug = True
 
@@ -23,9 +25,52 @@ def printTexts(stories):
  			print(title.get("data").get("selftext"))
 		print("----------")
 
+def MakeTextFrame(text):
+	img = Image.new('RGB', (1920, 1080), color = 'black')
+ 
+	d = ImageDraw.Draw(img)
+	d.text((10,10), text, fill= 'white')
+ 
+	return img
+
+def word_wrap(string, width=80, ind1=0, ind2=0, prefix=''):
+    """ word wrapping function.
+        string: the string to wrap
+        width: the column number to wrap at
+        prefix: prefix each line with this string (goes before any indentation)
+        ind1: number of characters to indent the first line
+        ind2: number of characters to indent the rest of the lines
+    """
+    string = prefix + ind1 * " " + string
+    newstring = ""
+    while len(string) > width:
+        # find position of nearest whitespace char to the left of "width"
+        marker = width - 1
+        while not string[marker].isspace():
+            marker = marker - 1
+
+        # remove line from original string and add it to the new string
+        newline = string[0:marker] + "\n"
+        newstring = newstring + newline
+        string = prefix + ind2 * " " + string[marker + 1:]
+
+    return newstring + string
+
+def GenerateFrames(title, selftext):
+	paragraph = word_wrap(selftext, width = 120)
+	counter = 0
+	if not(os.path.exists("temp/" + title)):
+		os.makedirs(("temp/" + title))
+	MakeTextFrame(paragraph).save("temp/" + title + "/" + title + str(counter) + ".png")
+	if debug:
+		print("made paragraph: " + str(counter) + " with length of: " + str(len(paragraph)))
+
 if __name__ == '__main__':
-	stories = json.loads(GetStories("tifu", "top", 10))
-	printTexts(stories)
+	stories = json.loads(GetStories("tifu", "top", 2))
+	if debug:
+		print("fetched " + str(NoOfStories(stories)) + " stories:")
+	for story in stories.get("data").get("children"):
+		GenerateFrames(story.get("data").get("title"), story.get("data").get("selftext"))
 
 
 	
